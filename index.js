26cf486123ca65a5a53ed465a0dc604d96d89edb
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-29 21:04:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
 }
 
-function nestedTarget(target){
+function nestedTarget(){
   return document.getElementById('nested').querySelector('div.target')
 
 }
